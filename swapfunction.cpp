d2bbf754c057e7b176ec8aeda5040b5bb8768f08
Priyanshu suwalka <128487169+Priyanshu0626@users.noreply.github.com>
2024-05-21 19:19:41 +0530

    Add files via upload

---
+++ swapfunction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	vector<int> myvector1 = { 11, 22, 33, 44 };
+	vector<int> myvector2 = { 55, 66, 77, 88 };
+	
+	//using swap () function to swap
+	//elements of vector
+	myvector1.swap(myvector2);
+	cout<<"\n"<<myvector2.at(3);
+	
+} //end of main
